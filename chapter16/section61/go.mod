1290e20951ad5a68ffd3c57b5d3bfcbbec5a9f5e
SangYeong Park <tkddud386@gmail.com>
2024-11-12 23:57:12 +0900

    chapter 61 완료

--- chapter16/section61/go.mod
+++ chapter16/section61/go.mod
@@ -1,6 +1,6 @@
-module github.com/Imsyp/go_todo_app/chapter16/section60
+module github.com/Imsyp/go_todo_app/chapter16/section61
 
-go 1.23.1
+go 1.23
 
 require golang.org/x/sync v0.8.0
 
--- chapter16/section61/main.go
+++ chapter16/section61/main.go
@@ -7,9 +7,12 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"golang.org/x/sync/errgroup"
-	"github.com/Imsyp/go_todo_app/chapter16/section60/config"
+	"github.com/Imsyp/go_todo_app/chapter16/section61/config"
 )
 
 func main() {
@@ -20,6 +23,8 @@ func main() {
 }
 
 func run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -33,6 +38,8 @@ func run(ctx context.Context) error {
 	s := &http.Server{
 		// 인수로 받은 net.Listener를 이용 -> Addr 필드 지정 X
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// 명령줄에서 테스트하기 위한 로직
+			time.Sleep(5 * time.Second)
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
