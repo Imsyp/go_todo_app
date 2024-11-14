e5883d52e9cc8e71cd79eb892b12996a45855c8a
SangYeong Park <tkddud386@gmail.com>
2024-11-14 21:10:35 +0900

    change: server test

--- chapter18/section72/server_test.go
+++ chapter18/section72/server_test.go
@@ -11,25 +11,25 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
+// main_test.go를 server_test.go로 변경
 func TestServer_Run(t *testing.T) {
-	l, err := net.Listen("tcp", "localhost:0")
+	l, err := net.Listen("tcp", "localhost:0") // net.Listen 함수를 사용하여 TCP 서버를 생성한다.
 	if err != nil {
 		t.Fatalf("failed to listen port %v", err)
 	}
 	ctx, cancel := context.WithCancel(context.Background())
 	eg, ctx := errgroup.WithContext(ctx)
-	mux := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	mux := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { // mux 변수를 http.HandlerFunc로 변경
+		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:]) // fmt.Fprintf 함수를 사용하여 응답을 작성한다.
 	})
 
 	eg.Go(func() error {
-		return run(ctx)
-		s := NewServer(l, mux)
-		return s.Run(ctx)
+		s := NewServer(l, mux) // NewServer 함수를 사용하여 서버를 생성한다.
+		return s.Run(ctx)      // Run 메서드를 사용하여 서버를 실행한다.
 	})
 	in := "message"
 	url := fmt.Sprintf("http://%s/%s", l.Addr().String(), in)
-	// // 어떤 포트로 리슨하고 있는지 확인
+
 	t.Logf("try request to %q", url)
 	rsp, err := http.Get(url)
 	if err != nil {
@@ -40,14 +40,14 @@ func TestServer_Run(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to read body: %v", err)
 	}
-	// http 서버의 반환값을 검증
+
 	want := fmt.Sprintf("Hello, %s!", in)
 	if string(got) != want {
 		t.Errorf("want %q, but got %q", want, got)
 	}
-	// run함수에 종료 알림을 전송
+
 	cancel()
-	// run함수의 반환값을 검증
+
 	if err := eg.Wait(); err != nil {
 		t.Fatal(err)
 	}
